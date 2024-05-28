f531b3667535a95b909fb2aebf98e2a9c433ddc3
Fiestagit config --global user.email pixelfiestastudios@gmail.com  git config --global user.name Fiesta <pixelfiestastudios@gmail.com>
2024-05-28 19:17:28 -0400

    Added Plaid integration for linking bank accounts and handling tokens

---
+++ finance_tracker/tracker/urls.py
@@ -0,0 +1,10 @@
+# tracker/urls.py
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('create_link_token/', views.create_link_token, name='create_link_token'),
+    path('exchange_public_token/', views.exchange_public_token, name='exchange_public_token'),
+]
